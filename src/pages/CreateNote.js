293e316ac65e4880ad168a349b8c842c40a98d8c
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2023-04-27 23:34:46 +0200

    create some components, edit note, create note, notes, noteItem

--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { IoIosArrowBack } from 'react-icons/io'
 
 const CreateNote = () => {
-  return (
-    <div>
-      huj
-    </div>
-  )
+	return (
+		<section>
+			<header className="create-note__header">
+				<Link to="/" className="btn">
+					<IoIosArrowBack />
+				</Link>
+				<button className="btn lg primary">Save</button>
+			</header>
+			<form className="create-note__form">
+				<input type="text" placeholder="Title" autoFocus />
+				<textarea rows="28" placeholder="Note Details..."></textarea>
+			</form>
+		</section>
+	)
 }
 
 export default CreateNote
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { CiSearch } from 'react-icons/ci'
+import dummyNotes from '../dummyNotes'
+import { Link } from 'react-router-dom'
+import { BsPlusLg } from 'react-icons/bs'
+import NoteItem from '../components/NoteItem'
 
 const Notes = () => {
 	return (
 		<section>
 			<header className="notes__header">
 				<h2 className="notes__heading">My Notes</h2>
-				<input className="notes__input" type="text" autoFocus placeholder="Keyword..." />
-				<button className="notes__btn">
+				{/* <input className="notes__input" type="text" autoFocus placeholder="Keyword..." /> */}
+				<button className="notes__btn btn">
 					<CiSearch />
 				</button>
 			</header>
-			<div className="notes__container"></div>
+			<div className="notes__container">
+				{dummyNotes.map(note => (
+					<NoteItem key={note.id} note={note} />
+				))}
+			</div>
+			<Link className="notes__btn btn add__btn">
+				<BsPlusLg />
+			</Link>
 		</section>
 	)
 }
