0fce07be62817eeb5d88fc41f43bed96ac7bb329
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2023-04-28 23:52:44 +0200

    logic in project finished

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,26 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CiSearch } from 'react-icons/ci'
-import dummyNotes from '../dummyNotes'
+import { MdClose } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import { BsPlusLg } from 'react-icons/bs'
 import NoteItem from '../components/NoteItem'
 
-const Notes = () => {
+const Notes = ({ notes }) => {
+	const [showSearch, setShowSearch] = useState(false)
+	const [text, setText] = useState('')
+	const [filteredNotes, setFilteredNotes] = useState(notes)
+
+	const handleSearch = () => {
+		setFilteredNotes(
+			notes.filter(note => {
+				if (note.title.toLowerCase().match(text.toLowerCase())) {
+					return note
+				}
+			})
+		)
+	}
+
+	useEffect(handleSearch, [text])
+
 	return (
 		<section>
 			<header className="notes__header">
-				<h2 className="notes__heading">My Notes</h2>
-				{/* <input className="notes__input" type="text" autoFocus placeholder="Keyword..." /> */}
-				<button className="notes__btn btn">
-					<CiSearch />
+				{!showSearch && <h2 className="notes__heading">My Notes</h2>}
+				{showSearch && (
+					<input
+						className="notes__input"
+						type="text"
+						value={text}
+						onChange={e => {
+							setText(e.target.value)
+							handleSearch()
+						}}
+						autoFocus
+						placeholder="Keyword..."
+					/>
+				)}
+				<button className="btn" onClick={() => setShowSearch(prevState => !prevState)}>
+					{showSearch ? <MdClose /> : <CiSearch />}
 				</button>
 			</header>
 			<div className="notes__container">
-				{dummyNotes.map(note => (
+				{filteredNotes.length == 0 && <p className='empty__notes'>Note notes found</p>}
+				{filteredNotes.map(note => (
 					<NoteItem key={note.id} note={note} />
 				))}
 			</div>
-			<Link className="notes__btn btn add__btn">
+			<Link to={'/create-note'} className="btn add__btn">
 				<BsPlusLg />
 			</Link>
 		</section>
